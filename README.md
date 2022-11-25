c3b0181876be234910a75472fd3bacbf1e22e911
hazenweng <hazenweng@tencent.com>
2022-11-25 14:51:57 +0800

    分拆

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # hook_dy_ssl
 
+## zijie 通用绕过 ssl pinning 脚本
+
 ```shell
 frida -U -f "com.ss.android.ugc.aweme" -l .\hook_dy_ssl.js
 ```
 
-zijie 通用绕过 ssl pinning 脚本
+## zijie 签名相关 java hook
+
+```shell
+frida -U -f "com.ss.android.ugc.aweme" -l .\hook_dy_sign.js
+```
--- hook_dy_ssl.js
+++ hook_dy_ssl.js
@@ -1,13 +1,6 @@
 function hook_ssl(){
 	var cronet = Module.findBaseAddress("libsscronet.so");
     console.log('libsscronet.so base', cronet);
-    // var pAddr = cronet.add(0x2BDA14);
-    // console.log(hexdump(pAddr, {
-    //     offset: 0,
-    //     length: 4,
-    //     header: true,
-    //     ansi: true,
-    // }));
 	var ver = Module.findExportByName("libttboringssl.so", "SSL_CTX_set_custom_verify");
 	var custom_verify = new NativeFunction(ver, 'pointer', ['pointer', 'int', 'pointer']);
 	var self = new NativeCallback(function(arg1, arg2, arg3) {
@@ -28,30 +21,6 @@ function hookCallBack(p){
 	Interceptor.replace(fun, self);
 }
 
-function hook_java() {
-    Java.perform(function(){  
-        let k = Java.use("ms.bd.c.k");
-        k["a"].implementation = function (i, i2, j, str, obj) {
-            if(i != 16777217) {
-                console.log('a is called' + ', ' + 'i: ' + i + ', ' + 'i2: ' + i2 + ', ' + 'j: ' + j + ', ' + 'str: ' + str + ', ' + 'obj: ' + obj);
-                if (i == 83886081) {
-                    var ArrayClz = Java.use("java.lang.reflect.Array");
-                    var len = ArrayClz.getLength(obj);
-                    for(let i=0;i!=len;i++){
-                        var cb = ArrayClz.get(obj,i).toString();
-                        console.log("ms.bd.c.k.a 83886081", cb);
-                    }
-                }
-            }
-            let ret = this.a(i, i2, j, str, obj);
-            if(i != 16777217) {
-                console.log('a ret value is ' + ret);
-            }
-            return ret;
-        };
-    });
-}
-
 function hook_native() {
     var android_dlopen_ext = Module.findExportByName(null, "android_dlopen_ext");
     console.log(android_dlopen_ext);
@@ -59,7 +28,6 @@ function hook_native() {
         Interceptor.attach(android_dlopen_ext,{
             onEnter: function(args){
                 var soName = args[0].readCString();
-                //console.log(soName);
                 if(soName.indexOf("libsscronet.so") != -1){
                     this.hook1 = true;
                 }
@@ -83,7 +51,6 @@ function hook_native() {
 
 function main() {
     hook_native();
-    hook_java();
 }
 
 setImmediate(main)
